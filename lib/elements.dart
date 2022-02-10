bef2bf68cf7ce914c0187b577a5fd7506e326d0e
Ohgyuchan <gyu3637@gmail.com>
2022-02-10 18:27:03 +0900

    update calendar

--- lib/elements.dart
+++ lib/elements.dart
@@ -5,8 +5,13 @@ class Elements {
   String group;
   final TextEditingController controller = TextEditingController();
   final FocusNode node = FocusNode();
+  DateTime dateTime;
   bool longPressed = false;
   bool checked = false;
 
-  Elements(this.group);
+  Elements(this.group, this.dateTime);
+}
+
+int getHashCode(DateTime key) {
+  return key.day * 1000000 + key.month * 10000 + key.year;
 }
